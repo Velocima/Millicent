9fed1b06744e086023f6a67b8a5100c46b0e9fd5
Velo <hartley.max@outlook.com>
2020-08-30 19:23:56 +0100

    menu nav bar formatted

--- Js/nav.js
+++ Js/nav.js
@@ -1,16 +1,24 @@
 const showMenu = () => {
     const burger = document.querySelector('.burger');
     const menu = document.querySelector('.menu');
+    const line = document.querySelectorAll('.line');
+
 
     burger.addEventListener('click', (e) => {
-        menu.classList.toggle('menu-active')
-        burger.classList.toggle('burger-active')
+        menu.classList.toggle('menu-active');
+        burger.classList.toggle('burger-active');
+        line[0].classList.toggle('line-active');
+        line[1].classList.toggle('line-active');
+        line[2].classList.toggle('line-active');
     });
 
     document.body.addEventListener('click', (e) => {
-        if(e.clientY>279 && menu.classList.value.includes('menu-active')) {
-        menu.classList.toggle('menu-active');
-        burger.classList.toggle('burger-active');
+        if(e.clientY > menu.clientHeight && menu.classList.value.includes('menu-active')) {
+            menu.classList.toggle('menu-active');
+            burger.classList.toggle('burger-active');
+            line[0].classList.toggle('line-active');
+            line[1].classList.toggle('line-active');
+            line[2].classList.toggle('line-active');
         }
     })
 }
